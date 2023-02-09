af3931923af71a454727e94933826c261b59dac2
WSMorrison <willimo@gmail.com>
2023-02-09 16:10:37 +0000

    Add eventview.html to view event details, with function to open events from index and location links from details.

--- registration/urls.py
+++ registration/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
+    path('<str:event_name>/', views.EventView.as_view(), name='event_view'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 
@@ -8,3 +8,15 @@ class EventList(generic.ListView):
     queryset = Event.objects.order_by('event_date')
     template_name = 'index.html'
     paginate_by = 10
+
+
+class EventView(View):
+
+    def get(self, request, event_name, *args, **kwargs):
+        event = get_object_or_404(Event, event_name=event_name)
+
+        return render(
+            request,
+            'eventview.html',
+            {'event': event}
+        )
