9d8a59e52c943cc0f5149cf7e45d0081e67e5240
WSMorrison <willimo@gmail.com>
2023-02-09 10:23:03 +0000

    Add basic admin functionality for car and site user models.

--- registration/admin.py
+++ registration/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Car, Siteuser
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     title = ('Event Date')
     list_filter = ('event_date',)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_filter = ('car_year',)
+
+
+@admin.register(Siteuser)
+class SiteuserAdmin(admin.ModelAdmin):
+    list_filter = ('event_organizer',)
