9e4d172197e729d0eeb65f9d6049d04decbaca0b
WSMorrison <willimo@gmail.com>
2023-02-09 12:58:39 +0000

    Add skeletonized html for main page.

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-# from .models import event
+from .models import Event
+
+
+class EventList(generic.ListView):
+    model = Event
+    queryset = Event.objects.order_by('event_date')
+    template_name = 'index.html'
+    paginate_by = 10
