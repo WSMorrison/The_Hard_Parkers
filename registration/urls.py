bca6ac6db149ba1b2628eb6b3f1d6224d391d5f7
WSMorrison <willimo@gmail.com>
2023-03-02 15:04:12 +0000

    Fix issue with eventreg.html displaying.

--- registration/urls.py
+++ registration/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
     path('<str:event_name>/', views.EventView.as_view(), name='event_view'),
-    path('<str:event_name>/eventreg/', views.EventView.as_view(), name='event_reg'),
+    path('<str:event_name>/eventreg/', views.EventReg.as_view(), name='event_reg'),
 ]
